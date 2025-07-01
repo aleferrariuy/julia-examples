b66cdd09e56b932894eb46a8a79ec4046ecdf52d
AlejandroFerrari <118323790+aleferrariuy@users.noreply.github.com>
2025-07-01 14:00:14 -0300

    plots.jl, Create
    Plotting
    
    Plotting in Julia is only possible with additional Packages. Examples of some of the main packages are given below.

---
+++ scripting/plots.jl
@@ -0,0 +1,10 @@
+using Plots
+# http://docs.juliaplots.org/latest/
+
+# plot some data
+plot([cumsum(rand(500) .- 0.5), cumsum(rand(500) .- 0.5)])
+
+# save the current figure
+savefig("plots.svg")
+# .eps, .pdf, & .png are also supported
+# we used svg here because it respects the width and height specified above
